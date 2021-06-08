c3ab1463c1705291886067329ea1c2c80ead2232
Francesco <francescococlite@gmail.com>
2021-06-08 13:37:45 +0200

    random pc

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+var numPc = [];
+
+
+for (var i = 0; i< 16; i++){
+    numPc.push(randomNumb(1,100));
+
+    if (numPc[i] != randomNumb(1,100))
+    console.log(numPc);
+
+}
+
+
+
+function randomNumb(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
