3c9e40a851f5a1371c978ef13375d79cf11dd1b2
Francesco <francescococlite@gmail.com>
2021-06-09 17:10:52 +0200

    mini grafica+ bonus

--- js/script.js
+++ js/script.js
@@ -3,67 +3,76 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-
-var numPc = [];
-var difficolta = parseInt(prompt('Seleziona una difficoltà da 0 a 2'))
+document.getElementById("start").addEventListener("click", function(){
 
 
-while (numPc.length < 16 && difficolta == 0) {
-    var numRan = (randomNumb(1,100));
-    if (!numPc.includes(numRan)) {
-        numPc.push(numRan); 
-    }
-}
-while (numPc.length < 16 && difficolta == 1) {
-    var numRan = (randomNumb(1,80));
-    if (!numPc.includes(numRan)) {
-        numPc.push(numRan); 
-    }
-}
-while (numPc.length < 16 && difficolta == 2) {
-    var numRan = (randomNumb(1,50));
-    if (!numPc.includes(numRan)) {
-        numPc.push(numRan); 
-    }
-}
-console.log(numPc);
-
-var listaUtente =[];
-
-var semaforo = true;
-i=0
-
-
-// inserire funzione con min e max
-while (i< (100-16) && semaforo){
-    var NumUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
+    var numPc = [];
+    var difficolta = parseInt(prompt('Seleziona una difficoltà da 0 a 2'))
     
-    if (listaUtente.includes(NumUtente)){
-        semaforo = false;
-        console.log('BOOM hai inserito due volte lo stesso numero');
-        console.log('Hai totalizzato ' + i + ' Punti' );
+    
+    while (numPc.length < 16 && difficolta == 0) {
+        var numRan = (randomNumb(1,100));
+        if (!numPc.includes(numRan)) {
+            numPc.push(numRan); 
+        }
     }
-
-    if (NumUtente != 0 && NumUtente <100 && !numPc.includes(NumUtente)) {
-        console.log(NumUtente);
-        listaUtente.push(NumUtente);
+    while (numPc.length < 16 && difficolta == 1) {
+        var numRan = (randomNumb(1,80));
+        if (!numPc.includes(numRan)) {
+            numPc.push(numRan); 
+        }
+    }
+    while (numPc.length < 16 && difficolta == 2) {
+        var numRan = (randomNumb(1,50));
+        if (!numPc.includes(numRan)) {
+            numPc.push(numRan); 
+        }
+    }
+    console.log(numPc);
+    
+    var listaUtente =[];
+    
+    var semaforo = true;
+    i=0
+    
+    
+    
+    while (i< (100-16) && semaforo){
+        var NumUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
         
-
-    }else{
-        semaforo = false;
-        console.log('BOOM');
-        console.log('Hai totalizzato ' + i + ' Punti' );
+        if (listaUtente.includes(NumUtente)){
+            semaforo = false;
+            console.log('BOOM hai inserito due volte lo stesso numero');
+            console.log('Hai totalizzato ' + i + ' Punti' );
+            document.getElementById('punteggio').innerHTML = i
+            document.getElementById('risultato').innerHTML = 'BOOM hai inserito due volte lo stesso numero';
+        }
+    
+        if (NumUtente != 0 && NumUtente <100 && !numPc.includes(NumUtente)) {
+            console.log(NumUtente);
+            listaUtente.push(NumUtente);
+            
+            
+    
+        }else{
+            semaforo = false;
+            console.log('BOOM');
+            console.log('Hai totalizzato ' + i + ' Punti' )
+            document.getElementById('punteggio').innerHTML = i
+            document.getElementById('risultato').innerHTML = 'BOOM hai volpito una bomba';
+        }
+    
+        i++;
     }
+    
+    
+    
+    
+    
+    
+    
+    function randomNumb(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+      }
 
-    i++;
-}
-
-
-
-
-
-
-
-function randomNumb(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+});
